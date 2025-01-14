ab46b59794bea27aa08a96ee62a418c80c5c17ce
unique-gautam-yadav <gautamyadav08112005@gmail.com>
2025-01-14 11:50:58 +0530

    initialize project structure with backend and client setup, add essential files and configurations

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./utils/db.js");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    name: 'in_share backend',
+    team: 'Group 28'
+  });
+});
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}/`);
+});
